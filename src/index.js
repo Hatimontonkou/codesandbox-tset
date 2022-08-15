65370c212686f3d411edf198d1d71d3ca02df450
Hatimontonkou <110019666+Hatimontonkou@users.noreply.github.com>
2022-08-15 13:19:02 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -155,29 +155,48 @@
 /**
  * mapやfilterを使った配列の処理
  */
-const nameArr = ["ルアン", "ゆや", "atsu"];
-for (let i = 0; i < nameArr.length; i++) {
-  console.log(`${i}番目は${nameArr[i]}です。`);
-}
+// const nameArr = ["ルアン", "ゆや", "atsu"];
+// for (let i = 0; i < nameArr.length; i++) {
+//   console.log(`${i}番目は${nameArr[i]}です。`);
+// }
 
-const nameArr2 = nameArr.map((name) => {
-  return name;
-});
-console.log(nameArr2);
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
 
-nameArr.map((name, i) => console.log(`${i}番目は${name}です。`));
+// nameArr.map((name, i) => console.log(`${i}番目は${name}です。`));
 
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 0;
-});
-console.log(newNumArr);
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "atsu") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "atsu") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件　？　条件がtrueの時：条件がfalseの時
+const val1 = 1 < 0 ? "true" : "false";
+console.log(val1);
+
+const num = "1300";
+console.log(num.toLocaleString());
+
+const formattedNum =
+  typeof num === "number" ? num.toLocaleString() : "数値を入力してください";
+console.log(formattedNum);
+
+const checkSum = (num1, num2) => {
+  return num1 + num2 > 100 ? "100を超えています" : "許容範囲です";
+};
+console.log(checkSum(50, 40));
